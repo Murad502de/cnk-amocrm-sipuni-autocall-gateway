e00d0107dd54b03414a0f2f60b28ee2539079375
itWelt <rassulovmurad@gmail.com>
2023-02-17 17:04:56 +0300

    [main][task] created App\Http\Controllers\API\V1\UserController1

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\V1\UserController1;
 
 Route::prefix('v1')->group(function () {
     Route::prefix('admin')->middleware('token')->group(function () {
         Route::prefix('auth')->group(function () {
-            //Route::post('/signin', [AuthController::class, 'signin'])->withoutMiddleware('token');
+            Route::post('/signin', [UserController1::class, 'signin'])->withoutMiddleware('token');
         });
     });
 });
