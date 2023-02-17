d2f2c710175e67fdf6540524faa5395b92462e45
itWelt <rassulovmurad@gmail.com>
2023-02-17 17:15:01 +0300

    [main][task] created App\Http\Requests\API\V1\UserSigninRequest and App\Exceptions\ForbiddenException

--- app/Http/Controllers/API/V1/UserController1.php
+++ app/Http/Controllers/API/V1/UserController1.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers\API\V1;
 
+use App\Exceptions\ForbiddenException;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\API\V1\UserSigninRequest;
+use App\Models\User;
 
 class UserController1 extends Controller
 {
-    //
+    public function signin(UserSigninRequest $request)
+    {
+        $user = User::where('email', $request->email)
+            ->where('password', md5($request->password))
+            ->first();
+
+        if ($user) {
+            return [
+                'token' => $user->access_token,
+                'uuid'  => $user->uuid,
+            ];
+        } else {
+            throw new ForbiddenException('Access denied.');
+        }
+    }
 }
---
+++ app/Http/Requests/API/V1/UserSigninRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\API\V1;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class UserSigninRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'email'    => 'required',
+            'password' => 'required',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json('invalid input data', Response::HTTP_BAD_REQUEST)
+        );
+    }
+}
