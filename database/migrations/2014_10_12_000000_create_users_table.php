b64e9e0c76f744d2bad82ed0698994a9b756d970
itWelt <rassulovmurad@gmail.com>
2023-02-17 15:24:09 +0300

    [main][task] added database/migrations/user_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('uuid')->nullable();
+            $table->string('email')->nullable()->index();
+            $table->string('password')->nullable();
+            $table->string('access_token')->nullable();
         });
     }
 
