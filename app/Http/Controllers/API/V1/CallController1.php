cdfd35330554277d5e5250eec6ec976f37b88095
itWelt <rassulovmurad@gmail.com>
2023-02-21 01:40:05 +0300

    [main][task] fixed commit

--- app/Http/Controllers/API/V1/CallController1.php
+++ app/Http/Controllers/API/V1/CallController1.php
@@ -3,23 +3,39 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\V1\CallCreateRequest1;
+use App\Http\Requests\API\V1\CallUpdateRequest1;
+use App\Http\Resources\API\V1\CallResource1;
+use App\Models\Call;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CallController1 extends Controller
 {
-    public function index (Request $request) {
-        return 'CallController1@index';
+    public function index(Request $request)
+    {
+        return CallResource1::collection(Call::all());
     }
-    public function create () {
-        return 'CallController1@create';
+    public function create(CallCreateRequest1 $request)
+    {
+        $call = Call::create($request->all());
+
+        return $call ? $call->uuid : null; //FIXME: put in resource
     }
-    public function read () {
-        return 'CallController1@read';
+    public function read(Call $call): CallResource1
+    {
+        return new CallResource1($call);
     }
-    public function update () {
-        return 'CallController1@update';
+    public function update(Call $call, CallUpdateRequest1 $request)
+    {
+        $call->update($request->all());
+
+        return response()->json(['message' => 'success by update'], Response::HTTP_OK);
     }
-    public function delete () {
-        return 'CallController1@delete';
+    public function delete(Call $call)
+    {
+        $call->delete();
+
+        return response()->json(['message' => 'success by delete'], Response::HTTP_OK);
     }
 }
