8338d4df28ddea28647164da13a7271223cdaca0
itWelt <rassulovmurad@gmail.com>
2023-02-21 22:11:55 +0300

    [main][task] added App\Http\Requests\API\V1\ServicesAmoCrmAuthRequest1

--- app/Http/Controllers/API/V1/ServicesAmoCrmController1.php
+++ app/Http/Controllers/API/V1/ServicesAmoCrmController1.php
@@ -3,14 +3,40 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\AmoCRM;
+use App\Services\amoAPI\amoAPIHub;
+use App\Http\Requests\API\V1\ServicesAmoCrmAuthRequest1;
 
 class ServicesAmoCrmController1 extends Controller
 {
-    public function signin() {
-        return 'signin';
+    public function signin(ServicesAmoCrmAuthRequest1 $request)
+    {
+        $authData = [
+            'client_id'     => $request->all()['client_id'],
+            'client_secret' => config('services.amoCRM.client_secret'),
+            'code'          => $request->all()['code'],
+            'redirect_uri'  => config('services.amoCRM.redirect_uri'),
+            'subdomain'     => config('services.amoCRM.subdomain'),
+        ];
+        $amo         = new amoAPIHub($authData);
+        $response    = $amo->auth();
+        $accountData = [
+            'client_id'     => $request->all()['client_id'],
+            'client_secret' => config('services.amoCRM.client_secret'),
+            'subdomain'     => $authData['subdomain'],
+            'access_token'  => $response['access_token'],
+            'redirect_uri'  => $authData['redirect_uri'],
+            'token_type'    => $response['token_type'],
+            'refresh_token' => $response['refresh_token'],
+            'when_expires'  => time() + (int) $response['expires_in'] - 400,
+        ];
+
+        AmoCRM::auth($accountData);
+
+        return response(['OK'], 200);
     }
-    public function signout() {
-        return 'signout';
+    public function signout()
+    {
+        return response(['OK'], 200);
     }
 }
