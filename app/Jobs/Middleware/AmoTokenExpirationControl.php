35cbb44f909c2fdcb3f85840a4d6c589d6499d0f
itWelt <rassulovmurad@gmail.com>
2023-02-25 20:03:44 +0300

    [main][task] added jobs

---
+++ app/Jobs/Middleware/AmoTokenExpirationControl.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Jobs\Middleware;
+
+use App\Models\AmoCRM;
+use App\Services\amoAPI\amoHttp\amoClient;
+use Illuminate\Support\Facades\Log;
+
+class AmoTokenExpirationControl
+{
+    public function handle($job, $next)
+    {
+        $client   = new amoClient();
+        $authData = AmoCRM::getAuthData();
+
+        if ($authData) {
+            Log::info(__METHOD__, ['AmoTokenExpirationControl']); //DELETE
+            Log::info(__METHOD__, [json_encode($authData)]); //DELETE
+
+            if (time() >= (int) $authData['when_expires']) {
+                Log::info(__METHOD__, ['access token expired']); //DELETE
+
+                $response = $client->accessTokenUpdate($authData);
+
+                if ($response['code'] >= 200 && $response['code'] < 204) {
+                    $accountData = [
+                        'client_id'     => $authData['client_id'],
+                        'client_secret' => $authData['client_secret'],
+                        'subdomain'     => $authData['subdomain'],
+                        'access_token'  => $response['body']['access_token'],
+                        'redirect_uri'  => $authData['redirect_uri'],
+                        'token_type'    => $response['body']['token_type'],
+                        'refresh_token' => $response['body']['refresh_token'],
+                        'when_expires'  => time() + (int) $response['body']['expires_in'] - 400,
+                    ];
+
+                    AmoCRM::auth($accountData);
+
+                    Log::info(__METHOD__, ['access token updated']); //DELETE
+
+                    $next($job);
+                } else {
+                    Log::info(__METHOD__, ['Login error with code: ' . $response['code']]); //DELETE
+
+                    $job->release();
+                }
+            } else {
+                Log::info(__METHOD__, ['access token ist not expired']); //DELETE
+
+                $next($job);
+            }
+        } else {
+            Log::info(__METHOD__, ['Login data not found']); //DELETE
+
+            $job->release();
+        }
+    }
+}
